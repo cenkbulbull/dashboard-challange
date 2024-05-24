240fe01f925c42f584992dc2bcf89be56c0db12b
Cenk Bülbül <95653055+cenkbulbull@users.noreply.github.com>
2024-05-24 15:22:24 +0300

    feat: variables, fonts, mixins defined

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `
+                @import "@/assets/scss/main.scss";
+                @import "@/assets/scss/base/variables";
+                @import "@/assets/scss/base/typography";
+                @import "@/assets/scss/base/mixins";
+            `,
+      },
+    },
+  },
   transpileDependencies: true,
 });

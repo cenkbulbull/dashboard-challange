9eb193ac22882721d5c81a03832f0cea599457e9
Cenk Bülbül <95653055+cenkbulbull@users.noreply.github.com>
2024-05-26 15:11:29 +0300

    feat: line atom created

---
+++ src/components/atoms/Line.vue
@@ -0,0 +1,23 @@
+<template>
+  <div :class="['line', direction]" :style="style"></div>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+import { useLineProps } from "@/composables/useLineProps";
+
+const props = defineProps(useLineProps().lineProps);
+const { style } = useLineProps(props);
+</script>
+
+<style lang="scss" scoped>
+.line {
+  &.horizontal {
+    display: block;
+  }
+  
+  &.vertical {
+    display: block;
+  }
+}
+</style>
---
+++ src/composables/useLineProps.js
@@ -0,0 +1,35 @@
+import { computed } from "vue";
+
+export function useLineProps(props) {
+  const lineProps = {
+    direction: {
+      type: String,
+      default: "horizontal",
+      validator: value => ['horizontal', 'vertical'].includes(value)
+    },
+    thickness: {
+      type: String,
+      default: "1px",
+    },
+    length: {
+      type: String,
+      default: "100%",
+    },
+    color: {
+      type: String,
+      default: "#000",
+    },
+    radius: {
+      type: String,
+    },
+  };
+
+  const style = computed(() => ({
+    borderRadius: props.radius,
+    backgroundColor: props.color,
+    width: props.direction === "horizontal" ? props.length : props.thickness,
+    height: props.direction === "horizontal" ? props.thickness : props.length,
+  }));
+
+  return { lineProps, style };
+}

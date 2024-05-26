75ac235f1c8a7661da8fb21bad2b4789a7e5caf5
Cenk Bülbül <95653055+cenkbulbull@users.noreply.github.com>
2024-05-26 18:47:49 +0300

    fix: button atom edited

--- src/components/atoms/Button.vue
+++ src/components/atoms/Button.vue
@@ -1,6 +1,11 @@
 <template>
   <button class="btn" :style="style">
-    <Icon v-if="props.icon" :name="props.icon" />
+    <Icon
+      v-if="props.icon"
+      :name="props.icon"
+      :size="props.iconSize"
+      :color="props.iconColor"
+    />
     {{ props.text }}
   </button>
 </template>
@@ -8,14 +13,17 @@
 <script setup>
 import Icon from "./Icon.vue";
 import { defineProps } from "vue";
-import { useCommonProps } from "@/composables/useCommonProps";
+import { useButtonProps } from "@/composables/useButtonProps";
 
-const props = defineProps(useCommonProps().commonProps);
-const { style } = useCommonProps(props);
+const props = defineProps(useButtonProps().buttonProps);
+const { style } = useButtonProps(props);
 </script>
 
 <style lang="scss" scoped>
 .btn {
+  display: flex;
+  align-items: center;
+  gap: 4px;
   border: 0;
   cursor: pointer;
 }
---
+++ src/composables/useButtonProps.js
@@ -0,0 +1,31 @@
+import { computed } from "vue";
+
+export function useButtonProps(props) {
+  const buttonProps = {
+    text: String,
+    icon: String,
+    bg: String,
+    color: String,
+    iconColor: String,
+    fontSize: String,
+    iconSize: String,
+    padding: String,
+    border: String,
+    radius: String,
+    width: String,
+    height: String,
+  };
+
+  const style = computed(() => ({
+    backgroundColor: props.bg,
+    color: props.color,
+    fontSize: props.fontSize,
+    padding: props.padding,
+    border: props.border,
+    borderRadius: props.radius,
+    width: props.width,
+    height: props.height,
+  }));
+
+  return { buttonProps, style };
+}

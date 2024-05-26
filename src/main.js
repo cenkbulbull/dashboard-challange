a278c0a5cb034e66c9853798d3ef93b3df0dcd74
Cenk Bülbül <95653055+cenkbulbull@users.noreply.github.com>
2024-05-26 18:30:17 +0300

    feat: bootstrap added to the project

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+// Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+// Bootstrap JS (Optional)
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 import 'remixicon/fonts/remixicon.css'
 
 createApp(App).use(router).mount("#app");

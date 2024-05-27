7d98afc90164cf0507977fcc345df96ecbf1786a
Cenk Bülbül <95653055+cenkbulbull@users.noreply.github.com>
2024-05-27 14:19:13 +0300

    fix: link item atom edited

--- src/components/atoms/LinkItem.vue
+++ src/components/atoms/LinkItem.vue
@@ -1,38 +1,39 @@
 <template>
-  <div class="link-item" :style="style">
+  <router-link class="link-item" :style="style" :to="props.to">
     <div v-if="props.icon" class="link-item__icon">
       <Icon :name="props.icon" />
     </div>
     <div class="link-item__text">
       {{ props.text }}
     </div>
-  </div>
+  </router-link>
 </template>
 
 <script setup>
 import Icon from "./Icon.vue";
 import { defineProps } from "vue";
-import { useCommonProps } from "@/composables/useCommonProps";
+import { useLinkItemProps } from "@/composables/useLinkItemProps";
 
-const props = defineProps(useCommonProps().commonProps);
-const { style } = useCommonProps(props);
+const props = defineProps(useLinkItemProps().linkItemProps);
+const { style } = useLinkItemProps(props);
 </script>
 
 <style lang="scss" scoped>
 .link-item {
   display: flex;
+  text-decoration: none;
 
   &__icon {
     margin-right: 8px;
   }
 
-  &--active{
+  &--active {
     background-color: $eerie-black-5;
   }
 
-  &:hover{
+  &:hover {
+    transition: all 0.3s;
     background-color: $eerie-black-5;
   }
-
 }
 </style>
---
+++ src/composables/useLinkItemProps.js
@@ -0,0 +1,30 @@
+import { computed } from "vue";
+
+export function useLinkItemProps(props) {
+  const linkItemProps = {
+    text: String,
+    icon: String,
+    to: String,
+    bg: String,
+    color: String,
+    fontSize: String,
+    padding: String,
+    border: String,
+    radius: String,
+    width: String,
+    height: String,
+  };
+
+  const style = computed(() => ({
+    backgroundColor: props.bg,
+    color: props.color,
+    fontSize: props.fontSize,
+    padding: props.padding,
+    border: props.border,
+    borderRadius: props.radius,
+    width: props.width,
+    height: props.height,
+  }));
+
+  return { linkItemProps, style };
+}

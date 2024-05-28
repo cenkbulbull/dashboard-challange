2c79d8d820afff98ad4f759e8716cb053afb6bdb
Cenk Bülbül <95653055+cenkbulbull@users.noreply.github.com>
2024-05-28 16:35:40 +0300

    feat: main js edited

--- src/main.js
+++ src/main.js
@@ -6,7 +6,33 @@ import router from "./router";
 import 'bootstrap/dist/css/bootstrap.min.css';
 // Bootstrap JS (Optional)
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+// Remixicon
+import 'remixicon/fonts/remixicon.css';
 
-import 'remixicon/fonts/remixicon.css'
+// Chartjs
+import { Chart as ChartJS, CategoryScale, RadialLinearScale, ArcElement, LinearScale, BarElement, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js';
+import { Bar, Line, Doughnut } from 'vue-chartjs';
 
-createApp(App).use(router).mount("#app");
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  PointElement,
+  LineElement,
+  RadialLinearScale,
+  ArcElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const app = createApp(App);
+
+app.use(router);
+
+// Chart
+app.component('Bar', Bar);
+app.component('Line', Line);
+app.component('Doughnut', Doughnut);
+
+app.mount("#app");

2c1471573b2798fde0ec51ada0ab8c63cb43b0fb
Cenk Bülbül <95653055+cenkbulbull@users.noreply.github.com>
2024-05-28 20:32:37 +0300

    feat: search props created

---
+++ src/composables/useSearchProps.js
@@ -0,0 +1,31 @@
+import { computed, defineProps } from "vue";
+
+export function useSearchProps(props) {
+  const searchProps = {
+    icon: String,
+    name: String,
+    placeholder: String,
+    bg: String,
+    color: String,
+    fontSize: String,
+    padding: String,
+    border: String,
+    radius: String,
+    width: String,
+    height: String,
+  };
+
+  const style = computed(() => ({
+    backgroundColor: props.bg,
+    color: props.color,
+    fontSize: props.fontSize,
+    padding: props.padding,
+    border: props.border,
+    borderRadius: props.radius,
+    width: props.width,
+    height: props.height,
+    "--placeholder-color": props.color,
+  }));
+
+  return { searchProps, style,defineProps };
+}

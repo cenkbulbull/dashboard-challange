735d6720e847278fbe40cec64641878d8e68267c
Cenk Bülbül <95653055+cenkbulbull@users.noreply.github.com>
2024-05-28 20:31:51 +0300

    feat: search component created

---
+++ src/components/Search.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="search" :style="style">
+    <div class="search__input">
+      <Icon :name="props.icon" :color="props.color"/>
+      <Input border="0" bg="transparent" :placeholder="props.placeholder" :fontSize="props.fontSize"/>
+    </div>
+    <div class="search__button">
+      <Button icon="hashtag" :color="props.color"/>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useSearchProps,defineProps } from "@/composables/useSearchProps";
+
+const props = defineProps(useSearchProps().searchProps);
+const { style } = useSearchProps(props);
+</script>
+
+<style lang="scss" scoped>
+.search{
+  display: flex;
+  align-items: center;
+  gap: 8px;
+
+  &__input{
+    display: flex;
+    gap: 4px;
+  }
+}
+</style>

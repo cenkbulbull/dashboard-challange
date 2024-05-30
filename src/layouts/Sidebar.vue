d2c4489694e7a254d18d5efdaf37d1692d0bc997
Cenk Bülbül <95653055+cenkbulbull@users.noreply.github.com>
2024-05-30 21:18:39 +0300

    feat: data was created and added for the sidebar

--- src/layouts/Sidebar.vue
+++ src/layouts/Sidebar.vue
@@ -7,16 +7,16 @@
         width="24px"
         height="24px"
       />
-      <span class="sidebar__header-title"><Text text="Bordo.io" /></span>
+      <span class="sidebar__header-title"><Text text="Bordo.io" fontSize="14px" fontWeight="400" lineHeight="20px"/></span>
     </div>
 
     <div class="sidebar__pages">
       <span class="sidebar__pages-title"><Text text="Pages" /></span>
       <LinkItem
-        v-for="i in 5"
-        :key="i"
-        to="/"
-        text="Overview"
+        v-for="link in links.pages"
+        :key="link.id"
+        :to="link.to"
+        :text="link.text"
         padding="8px"
         radius="12px"
         color="#1c1c1c"
@@ -26,11 +26,11 @@
     <div class="sidebar__dashboards">
       <span class="sidebar__dashboards-title"><Text text="Dashboard" /></span>
       <LinkItem
-        v-for="i in 10"
-        :key="i"
-        text="Projects"
-        to="/"
-        icon="mail-check-fill"
+        v-for="link in links.dashboards"
+        :key="link.id"
+        :text="link.text"
+        :to="link.to"
+        :icon="link.icon"
         padding="8px"
         radius="12px"
         color="#1c1c1c"
@@ -39,3 +39,7 @@
     
   </div>
 </template>
+
+<script setup>
+import links from '@/data/links/sidebarlinks.json'
+</script>

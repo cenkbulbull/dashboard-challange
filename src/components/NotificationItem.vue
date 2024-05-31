47a181654f6cf38faa8de01a3cfdd006f9109d92
Cenk Bülbül <95653055+cenkbulbull@users.noreply.github.com>
2024-05-31 16:50:04 +0300

    feat: notification items were created and timelined

---
+++ src/components/NotificationItem.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="notification" :style="style">
+    <div class="notification__avatar">
+      <Avatar
+        :bg="props.avatarBg"
+        :icon="props.icon"
+        :image="props.avatarImage"
+        :width="props.avatarWidth"
+        :height="props.avatarHeight"
+        :radius="props.avatarRadius"
+      />
+    </div>
+
+    <div class="notification__info">
+      <div class="notification__info-text">
+        <Text :text="props.text" />
+      </div>
+
+      <div class="notification__info-date">
+        <Text :text="props.dateText" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import {
+  useNotificationItemProps,
+  defineProps,
+} from "@/composables/ui/useNotificationItemProps";
+
+const props = defineProps(useNotificationItemProps().notificationItemProps);
+const { style } = useNotificationItemProps(props);
+</script>
---
+++ src/composables/ui/useNotificationItemProps.js
@@ -0,0 +1,35 @@
+import { computed, defineProps } from "vue";
+
+export function useNotificationItemProps(props) {
+  const notificationItemProps = {
+    text: String,
+    icon: String,
+    bg: String,
+    avatarBg: String,
+    color: String,
+    fontSize: String,
+    avatarImage: String,
+    padding: String,
+    border: String,
+    radius: String,
+    avatarRadius: String,
+    width: String,
+    height: String,
+    avatarWidth: String,
+    avatarHeight: String,
+    dateText: String,
+  };
+
+  const style = computed(() => ({
+    backgroundColor: props.bg,
+    color: props.color,
+    fontSize: props.fontSize,
+    padding: props.padding,
+    border: props.border,
+    borderRadius: props.radius,
+    width: props.width,
+    height: props.height,
+  }));
+
+  return { notificationItemProps, style, defineProps };
+}

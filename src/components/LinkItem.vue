8dd579027240e0c77b12927c4b44cd0d922cf2e8
Cenk Bülbül <95653055+cenkbulbull@users.noreply.github.com>
2024-06-01 15:45:29 +0300

    feat: link item component created

---
+++ src/components/LinkItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <router-link class="link-item" :style="style" :to="props.to">
+
+    <div v-if="props.icon" class="link-item__icon">
+      <Icon :name="props.icon" />
+    </div>
+
+    <div class="link-item__text">
+      <Text
+        :text="props.text"
+        :fontWeight="props.fontWeight"
+        :fontSize="props.fontSize"
+        :lineHeight="props.lineHeight"
+      />
+    </div>
+    
+  </router-link>
+</template>
+
+<script setup>
+import {
+  useLinkItemProps,
+  defineProps,
+} from "@/composables/ui/useLinkItemProps";
+
+const props = defineProps(useLinkItemProps().linkItemProps);
+const { style } = useLinkItemProps(props);
+</script>
---
+++ src/composables/ui/useLinkItemProps.js
@@ -0,0 +1,32 @@
+import { computed, defineProps } from "vue";
+
+export function useLinkItemProps(props) {
+  const linkItemProps = {
+    text: String,
+    icon: String,
+    to: String,
+    bg: String,
+    color: String,
+    fontSize: String,
+    padding: String,
+    border: String,
+    radius: String,
+    width: String,
+    height: String,
+    lineHeight: String,
+    fontWeight: String
+  };
+
+  const style = computed(() => ({
+    backgroundColor: props.bg,
+    color: props.color,
+    fontSize: props.fontSize,
+    padding: props.padding,
+    border: props.border,
+    borderRadius: props.radius,
+    width: props.width,
+    height: props.height,
+  }));
+
+  return { linkItemProps, style, defineProps };
+}

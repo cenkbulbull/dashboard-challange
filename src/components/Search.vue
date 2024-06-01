f8054b673b068824438b0329626610b60d06520b
Cenk Bülbül <95653055+cenkbulbull@users.noreply.github.com>
2024-06-01 15:43:36 +0300

    refactor: search component updated

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="search" :style="style">
+
     <div class="search__icon">
       <Icon :name="props.icon" :color="props.color"/>
     </div>
+
     <div class="search__input">
       <Input border="0" bg="transparent" :placeholder="props.placeholder" :fontSize="props.fontSize"/>
     </div>
+    
   </div>
+
 </template>
 
 <script setup>
